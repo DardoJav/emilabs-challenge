50c3510dde11319fe07c0673d4d01cae7886abb9
MSI\Dardo <dardo.luna@gmail.com>
2025-01-21 09:48:37 -0300

    changed database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+export const sequelize = new Sequelize('mysql://root:ztZcgbuDSQAxanuQZgrErBjNCxPfzQxh@roundhouse.proxy.rlwy.net:16829/railway',
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
